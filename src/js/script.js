5db44fafeb0a2f4dd12a561fdb32c6cf68bb1751
Andrey Shurygin <7093553@gmail.com>
2021-01-26 10:49:07 +0300

    test two screen

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,37 @@ jQuery(document).ready(function($) {
   });
 
 });
+
+docSlider.init({
+  beforeChange : function(index){
+    var indexPage = index;
+    if(indexPage == 0) {
+      $('.aside__nav-child').addClass('aside__nav-child_service');
+      $('.aside__nav-link--active').css('text-decoration','underline');
+    }
+    else {
+      $('.aside__nav-child').removeClass('aside__nav-child_service');
+      $('.aside__nav-link--active').css('text-decoration','none');
+    }
+  },
+  speed : 600,
+  startSpeed : null,
+  easing : 'ease',
+  scrollReset : false,
+  pager: true
+});
+
+
+/*docSlider.init({
+  beforeChange : function(page){
+    var x = page;
+    console.log(x);*/
+    /*if(x == 0) {
+      $('.aside__nav-child').addClass('aside__nav-child_service');
+    }
+    else {
+      $('.aside__nav-child').removeClass('aside__nav-child_service');
+    }*/
+
+  /*}
+});*/
