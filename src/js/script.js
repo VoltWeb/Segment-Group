caa541a4783842ef428375bc38c8ca5b134d1866
Andrey Shurygin <7093553@gmail.com>
2021-01-29 18:20:30 +0300

    js, bugs, adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -17,11 +17,11 @@ docSlider.init({
     var indexPage = pager;
     console.log(pager);
     if(pager == 0) {
-      $('.aside__nav-child').addClass('aside__nav-child_service');
+      //$('.aside__nav-child').addClass('aside__nav-child_service');
       $('.aside__nav-link--active').css('text-decoration','underline');
     }
     else {
-      $('.aside__nav-child').removeClass('aside__nav-child_service');
+      //$('.aside__nav-child').removeClass('aside__nav-child_service');
       $('.aside__nav-link--active').css('text-decoration','none');
     }
   },
@@ -29,19 +29,85 @@ docSlider.init({
   startSpeed : null,
   easing : 'ease',
   scrollReset : false,
-  pager: true
+  pager: false
 });
 
-/*docSlider.init({
-  beforeChange : function(page){
-    var x = page;
-    console.log(x);*/
-    /*if(x == 0) {
-      $('.aside__nav-child').addClass('aside__nav-child_service');
+$('.about').owlCarousel({
+  nav: true,
+  loop: false,
+  dots: false,
+  margin: 0,
+  responsive: {
+    1200: {
+      items: 1
     }
-    else {
-      $('.aside__nav-child').removeClass('aside__nav-child_service');
-    }*/
+  },
+  navText: [
+    '<span class="nav-owl-btn nav-owl-btn_left"><svg class="nav-owl-btn nav-owl-btn_left nav-owl-icon" width="15" height="15"><use xlink:href="#left-chevron"></use></svg></span>',
+    '<span class="nav-owl-btn nav-owl-btn_right"><svg class="nav-owl-btn nav-owl-btn_left nav-owl-icon" width="15" height="15"><use xlink:href="#left-chevron"></use></svg></span>'
+  ]
+});
 
-  /*}
+/*$('#page-3').owlCarousel({
+  nav: true,
+  loop: false,
+  dots: false,
+  margin: 0,
+  responsive: {
+    1200: {
+      items: 1
+    }
+  },
+  navText: [
+    '<span class="nav-owl-btn nav-owl-btn_left"><svg class="nav-owl-btn nav-owl-btn_left nav-owl-icon" width="15" height="15"><use xlink:href="#left-chevron"></use></svg></span>',
+    '<span class="nav-owl-btn nav-owl-btn_right"><svg class="nav-owl-btn nav-owl-btn_left nav-owl-icon" width="15" height="15"><use xlink:href="#left-chevron"></use></svg></span>'
+  ]
 });*/
+
+$('.exeprt-carousel').owlCarousel({
+  nav: true,
+  loop: false,
+  dots: false,
+  margin: 0,
+  responsive: {
+    1200: {
+      items: 1
+    }
+  },
+  navText: [
+    '<span class="nav-owl-btn nav-owl-btn_left"><svg class="nav-owl-btn nav-owl-btn_left nav-owl-icon" width="15" height="15"><use xlink:href="#left-chevron"></use></svg></span>',
+    '<span class="nav-owl-btn nav-owl-btn_right"><svg class="nav-owl-btn nav-owl-btn_left nav-owl-icon" width="15" height="15"><use xlink:href="#left-chevron"></use></svg></span>'
+  ]
+});
+
+$('.planning-carousel').owlCarousel({
+  nav: true,
+  loop: false,
+  dots: false,
+  margin: 0,
+  responsive: {
+    1200: {
+      items: 1
+    }
+  },
+  navText: [
+    '<span class="nav-owl-btn nav-owl-btn_left"><svg class="nav-owl-btn nav-owl-btn_left nav-owl-icon" width="15" height="15"><use xlink:href="#left-chevron"></use></svg></span>',
+    '<span class="nav-owl-btn nav-owl-btn_right"><svg class="nav-owl-btn nav-owl-btn_left nav-owl-icon" width="15" height="15"><use xlink:href="#left-chevron"></use></svg></span>'
+  ]
+});
+
+$('.objects').owlCarousel({
+  nav: true,
+  loop: false,
+  dots: false,
+  margin: 0,
+  responsive: {
+    1200: {
+      items: 1
+    }
+  }/*,
+  navText: [
+    '<span class=""><svg class="" width="127" height="15"><use xlink:href="#object-left"></use></svg></span>',
+    '<span class=""><svg class="" width="127" height="15"><use xlink:href="#object-right"></use></svg></span>'
+  ]*/
+});
